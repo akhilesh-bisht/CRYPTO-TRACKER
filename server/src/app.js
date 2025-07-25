2de25be22d20628d29e6a8419859502fd75048db
Akhilesh Bisht <akhileshbisht2020@gmail.com>
2025-07-25 12:51:50 +0530

    bakcend

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,22 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 
 
+// Enable CORS with specific origin and credentials
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "*",
+    credentials: true,
+  })
+);
+
+//routes import 
 
 import coinsRouter from './routes/coin.route.js'
+
+// routes declations
 app.use('/api/coins', coinsRouter);
 
 export default app;
